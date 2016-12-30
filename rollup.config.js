aa1b3cd970f5d2707e6768e0aed7796045d84e64
Rich-Harris <richard.a.harris@gmail.com>
2016-12-29 19:01:58 -0500

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,9 @@ import json from 'rollup-plugin-json';
 export default {
 	entry: 'src/index.js',
 	plugins: [ json(), buble() ],
+	external: [ 'events', 'fs' ],
 	targets: [
 		{ dest: 'dist/rollup-watch.cjs.js', format: 'cjs' },
-		{ dest: 'dist/rollup-watch.es6.js', format: 'es6' }
+		{ dest: 'dist/rollup-watch.es.js', format: 'es' }
 	]
 };
