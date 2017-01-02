92fed634488598b3ccdfe4473c831cef3eed1714
porsager <rasmus@porsager.com>
2017-01-02 14:39:41 +0100

    Use chokidar if available

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import json from 'rollup-plugin-json';
 export default {
 	entry: 'src/index.js',
 	plugins: [ json(), buble({ target: { node: 4 } }) ],
-	external: [ 'events', 'fs', 'path' ],
+	external: [ 'events', 'fs', 'path', 'require-relative' ],
 	targets: [
 		{ dest: 'dist/rollup-watch.cjs.js', format: 'cjs' },
 		{ dest: 'dist/rollup-watch.es.js', format: 'es' }
